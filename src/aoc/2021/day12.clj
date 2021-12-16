7071c44b76b50ebfaa4f54df18022291dd206a7b
spring <wenchuan.lin@abagile.com>
2021-12-16 22:56:51 +0800

    Day 12: third attempt still fails
    Can't pass third example and input.

--- src/aoc/2021/day12.clj
+++ src/aoc/2021/day12.clj
@@ -25,22 +25,16 @@
           {}
           pairs))
 
+(defn visited? [path node]
+  (some #(= node %) path))
 
-(defn path-with-only-once-small-cave? [path small-caves]
-  (->> small-caves
-       (map (frequencies path))
-       (remove nil?)
-       (every? #(<= % 1))))
-
-(defn all-paths [cave-system path]
-  ;; (prn (partition 2 1 path))
-  ;; (prn "start" (peek path))
-  (let [start (peek path)]
-    (->> (start cave-system)
-         (filter #(not-any? (fn [edge] (= edge [start %])) (partition 2 1 path)))
-         (mapcat #(all-paths cave-system (conj path %)))
-         (cons path)
-         (filter #(= :end (last %))))))
+(defn all-paths [cave-system small-caves path]
+  (let [current (peek path)]
+    (if (= current :end)
+      (vector path)
+      (->> (current cave-system)
+           (remove #(and (small-caves %) (visited? path %)))
+           (mapcat #(all-paths cave-system small-caves (conj path %)))))))
 
 (comment
   (cons [:start :12] [:12])
@@ -49,27 +43,24 @@
 (defn small-caves [cave-system]
   (->> (keys cave-system)
        (remove #(or (= % :start) (= % :end)))
-       (filter #(= (str %) (str/lower-case %)))))
-
+       (filter #(= (str %) (str/lower-case %)))
+       (set)))
 
 (defn part1 [edges]
   (let [pairs (->pairs edges)
         cave-system (->cave-system pairs)
         small-caves (small-caves cave-system)]
-    (prn cave-system)
-    (->> (all-paths cave-system [:start])
-         (filter #(path-with-only-once-small-cave? % small-caves))
+    (prn small-caves)
+    (->> (all-paths cave-system small-caves [:start])
          (count))))
-
 
 (defn part2 [edges])
 
-(comment)
-    ; => first attempt: 1691
-    ;; (part1 input 100))
-
-    ; => 216
-    ;; (part2 input 100))
+(defn parse-graph [lines]
+  (apply merge-with into
+         (for [line lines
+               :let [[a b] (str/split line #"-")]]
+           {a [b], b [a]})))
 
 (def example-1
   ["start-A" "start-b" "A-c" "A-b" "b-d" "A-end" "b-end"])
@@ -82,7 +73,20 @@
   "pj-he" "RW-he" "fs-DX" "pj-RW" "zg-RW" "start-pj" "he-WI" "zg-he"
   "pj-fs" "start-R"])
 
+(comment
+  (partition 2 1 [:c :b :d])
+  (part1 input)
+  (->cave-system (->pairs example-3))
+  (parse-graph example-3))
+
+    ; => first attempt: 1691
+    ;; (part1 input 100))
+
+    ; => 216
+    ;; (part2 input 100))
+
+
 (deftest test-example
-  (is (= 10 (part1 example-1)))
-  (is (= 19 (part1 example-2))))
-  ;; (is (= 226 (part1 example-3))))
+  ;; (is (= 10 (part1 example-1)))
+  ;; (is (= 19 (part1 example-2)))
+  (is (= 226 (part1 example-3))))
