dd08410e652c2bd2a188d1c6ed76024137806969
spring <wenchuan.lin@abagile.com>
2022-12-03 16:55:21 +0800

    2021 Day 21: Difficult one, still confused...

--- src/aoc/2021/day21.clj
+++ src/aoc/2021/day21.clj
@@ -1,18 +1,13 @@
-; [Day 17 - Advent of Code 2021](https://adventofcode.com/2021/day/17)
-(ns aoc.2021.day17
+; [Day 21 - Advent of Code 2021](https://adventofcode.com/2021/day/21)
+(ns aoc.2021.day21
   (:require
-    [aoc.util :as util]
     [clojure.test :refer [deftest is]]))
 
 (def example
-  ["Player 1 starting position: 4" "Player 2 starting position: 8"])
+  [4 8])
 
 (def input
-  ["Player 1 starting position: 10" "Player 2 starting position: 2"])
-
-(defn parse [input]
-  (->> (util/->integers input)))
-
+  [10 2])
 
 (defn part1 [[p1-start-pos p2-start-pos]]
   ; one outcome
@@ -41,35 +36,35 @@
                            (partition 2 2)))]
       (* (:p2-score result) (- (* 6 (:round result)) 3))))
 
-(defn part2 [input])
-  ; three dices (1, 2, 3)
-  ; multiple outcomes
+(def roll-counts
+  (->> (for [a (range 1 4)
+             b (range 1 4)
+             c (range 1 4)]
+         (+ a b c))
+       frequencies))
 
-(comment
-  (some #(= % 1000) [1000 813])
-  (part1 [4 8])
-  (part1 [10 2])
-  (part1 input)
+(def wins
+  (memoize
+   (fn [[p1 p2] [s1 s2] first?]
+     (->> (for [[roll n] roll-counts]
+            (let [np (mod (+ p1 roll) 10)
+                  ns (+ s1 np 1)]
+              (if (<= 21 ns)
+                {first? n}
+                (-> (wins [p2 np] [s2 ns] (not first?))
+                    (update-vals #(* n %))))))
+          (reduce (partial merge-with +))))))
 
-  (part2 input)
-
-  (->> (range 1 (inc 100))
-       (repeat)
-       (apply concat)
-       (partition 3)
-       (map #(apply + %))
-       (partition 2 2)))
+(defn part2 [input]
+  (prn input)
+  (->> (wins input [0 0] true)
+       vals
+       (reduce max)))
 
 (deftest test-example
-  (is (= 739785 (part1 [4 8])))
+  (is (= 739785 (part1 example)))
   (is (= 444356092776315 (part2 example))))
 
 (comment
-  (rem 12 11)
-  (util/->integers input)
-  (->> (partition 3 3 (range 1 (inc 100)))
-       (map #(apply + %))
-       (partition 2 1))
-  [(range 20 (inc 30)) (range -10 (inc -5))]
-  (apply min (range 20 (inc 30)))
-  (part1 example))
+  (time (part1 input)) ; 916083, "Elapsed time: 1.0745 msecs"
+  (time (part2 input))) ; 49982165861983, "Elapsed time: 0.187959 msecs"
