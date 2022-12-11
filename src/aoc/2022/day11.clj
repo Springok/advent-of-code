a32ded9f7c4c11ba2b12a40c02b0007a726c1a63
spring <wenchuan.lin@abagile.com>
2022-12-11 21:10:22 +0800

    2022 Day 11: Solved Part2 with help and cleanup code
    WTF is Chinese Remainder Theorem....

--- src/aoc/2022/day11.clj
+++ src/aoc/2022/day11.clj
@@ -37,12 +37,15 @@
   (parse-monkeys example)
   (parse-monkeys input))
 
-(defn take-turn [monkeys nth-monkey state]
+(defn take-turns [monkeys nth-monkey state partx]
   (let [monkey (nth monkeys nth-monkey)
         items  (nth (:items state) nth-monkey)
-        {:keys [op t-number tt tf]} monkey]
+        {:keys [op t-number tt tf]} monkey
+        crt-product (reduce * (map :t-number monkeys))]
     (reduce (fn [state item]
-              (let [w-level (bigint (/ (op item) 3))
+              (let [w-level (if (= partx :part1)
+                              (bigint (/ (op item) 3))
+                              (bigint (mod (op item) crt-product)))
                     to-nth-monkey (if (zero? (mod w-level t-number)) tt tf)]
                 (-> state
                     (update :items #(update (vec %) to-nth-monkey conj w-level))
@@ -51,32 +54,28 @@
      state
      items)))
 
-(defn turns [monkeys rounds]
-   (take (* (count monkeys) rounds) (cycle (range (count monkeys)))))
-
-(defn init-state [monkeys]
-  {:items (map :items monkeys)
-   :inspecting-times (vec (repeat (count monkeys) 0))})
-
-(defn process [monkeys turns initial-state]
+(defn take-rounds [monkeys turns initial-state partx]
   (loop [turns turns
          state initial-state]
-    (let [nth-monkey (first turns)]
-      (when (= nth-monkey 0) (prn "state" (:inspecting-times state)))
-      (if (= 0 (count turns))
-         state
-         (recur (rest turns) (take-turn monkeys nth-monkey state))))))
+    (if (= 0 (count turns))
+      state
+      (recur (rest turns) (take-turns monkeys (first turns) state partx)))))
+
+(defn monkey-business [monkeys rounds partx]
+  (let [init-state {:items (map :items monkeys)
+                    :inspecting-times (vec (repeat (count monkeys) 0))}
+        turns (take (* (count monkeys) rounds) (cycle (range (count monkeys))))]
+   (->> (-> (take-rounds monkeys turns init-state partx)
+            (:inspecting-times))
+        (sort >)
+        (take 2)
+        (apply *))))
 
 (defn part1 [input]
-  (let [monkeys (parse-monkeys input)
-        init-state (init-state monkeys)
-        turns (turns monkeys 20)]
-    (->> (:inspecting-times (process monkeys turns init-state))
-         (sort >)
-         (take 2)
-         (apply *))))
+  (monkey-business (parse-monkeys input) 20 :part1))
 
-(defn part2 [input])
+(defn part2 [input]
+  (monkey-business (parse-monkeys input) 10000 :part2))
 
 (deftest test-example
   (is (= 10605 (part1 example)))
@@ -84,4 +83,4 @@
 
 (comment
   (time (part1 input))  ; 57348, "Elapsed time: 3.165958 msecs"
-  (time (part2 input))) ; "Elapsed time: 1.037208 msecs"
+  (time (part2 input))) ; 14106266886, "Elapsed time: 39103.882917 msecs"
