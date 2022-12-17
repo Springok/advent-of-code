390284db6b2780991107018156a5c15825829b8d
spring <wenchuan.lin@abagile.com>
2022-12-18 00:40:12 +0800

    2022 Day 13

---
+++ src/aoc/2022/day13.clj
@@ -0,0 +1,71 @@
+(ns aoc.2022.day13
+  (:require
+    [aoc.util :as util]
+    [clojure.string :as str]
+    [clojure.test :refer [deftest is]]))
+
+(def example
+  (util/read-chunks "../resources/aoc/2022/day13-ex.txt"))
+
+(def input
+  (util/read-chunks "../resources/aoc/2022/day13.txt"))
+
+(defn parse-input [input]
+  (->> input
+       (map #(str/split % #"\n"))
+       (flatten)
+       (map read-string)))
+
+(defn compare-ba [l r]
+  (cond
+    (every? number? [l r]) (compare l r)
+    (number? l) (recur [l] r)
+    (number? r) (recur l [r])
+    :else (or (->> (map compare-ba l r)
+                   (drop-while zero?)
+                   (first))
+              (- (count l) (count r)))))
+
+(defn part1 [input]
+  (->> (parse-input input)
+       (partition 2)
+       (map #(apply compare-ba %))
+       (map-indexed (fn [idx result] (if (neg? result) (inc idx) 0)))
+       (apply +)))
+
+(defn part2 [input]
+  (->> (concat [[[2]] [[6]]] (parse-input input))
+       (sort compare-ba)
+       (map-indexed (fn [idx packet] (case packet
+                                      [[2]] (inc idx)
+                                      [[6]] (inc idx)
+                                      1)))
+       (apply *)))
+
+(defn -main [& _]
+  (println "part 1:" (time (part1 input)))
+  (println "part 2:" (time (part2 input))))
+
+(deftest test-example
+  (is (= 13 (part1 example)))
+  (is (= 140 (part2 example)))
+  (is (= -1 (compare-ba 1 [2])))
+  (is (= -1 (compare-ba [1 1 3] [1 1 5])))
+  (is (=  1 (compare-ba 9 [8 7 6])))
+  (is (= -2 (compare-ba [[4 4] 4 4] [[4 4] 4 4 4 4])))
+  (is (= -1 (compare-ba [[4 4] 4 4] [[4 4] 4 4 4])))
+  (is (= -1 (compare-ba [[1],[2,3,4]] [[1] 4])))
+  (is (=  1 (compare-ba [9] [8 7 6])))
+  (is (= -1 (compare-ba [4 4] [4 4 4])))
+  (is (=  0 (compare-ba [4 4] [4 4])))
+  (is (= -1 (compare-ba [] [3])))
+  (is (=  1 (compare-ba [7 7 7 7] [7 7 7])))
+  (is (=  1 (compare-ba [[[]]] [[]])))
+  (is (=  1 (compare-ba [1,[2,[3,[4,[5,6,7]]]],8,9] [1,[2,[3,[4,[5,6,0]]]],8,9]))))
+
+(comment
+  (-main))
+; (out) "Elapsed time: 3.437083 msecs"
+; (out) part 1: 6076
+; (out) "Elapsed time: 5.087667 msecs"
+; (out) part 2: 24805
